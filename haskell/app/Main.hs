66350048b1ed9bf31d1924950d6d6e6921732b49
jali-clarke <jinnah.ali-clarke@outlook.com>
2022-05-16 02:33:12 +0000

    add ghc version reporting to haskell executable, add initial overlay as overlays.default, rename deprecated defaultPackage and devShell attrs

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,4 +1,6 @@
+import qualified Data.Version as Version
 import Hello
+import qualified System.Info as Info
 
 main :: IO ()
-main = putStrLn helloString
+main = putStrLn $ helloString <> " (from compiler " <> Info.compilerName <> " " <> Version.showVersion Info.fullCompilerVersion <> ")"
--- haskell/flake.nix
+++ haskell/flake.nix
@@ -3,13 +3,18 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (
+    {
+      overlays.default = final: prev: rec {
+        hello-world = haskellPackages.hello-world;
+
+        haskellPackages = prev.haskellPackages // {
+          hello-world = final.haskellPackages.callCabal2nix "hello-world" ./. { };
+        };
+      };
+    } // flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
-
-        # can also use pkgs.haskell.packages.ghcXXX for a specific ghc version
-        ghcPkgs = pkgs.haskellPackages;
+        pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
 
         cabalWrapped = pkgs.writeShellScriptBin "cabal" ''
           ${pkgs.hpack}/bin/hpack && exec ${pkgs.cabal-install}/bin/cabal "$@"
@@ -19,17 +24,12 @@
           shopt -s globstar
           ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt . && ${pkgs.ormolu}/bin/ormolu -i {app,src,test}/**/*.hs
         '';
-
-        hello-world = ghcPkgs.callCabal2nix "hello-world" ./. { };
       in
       {
-        defaultPackage = hello-world;
-        packages = {
-          inherit hello-world;
-        };
+        packages.default = pkgs.hello-world;
 
-        devShell = pkgs.mkShell {
-          inputsFrom = [ hello-world.env ];
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ pkgs.hello-world.env ];
           packages = [
             cabalWrapped
             format-all
