8155fb57f1d3ba2426ee458edc7c912f1216b5b9
jali-clarke <jinnah.ali-clarke@outlook.com>
2022-09-12 10:19:11 -0400

    simplify haskell flake.nix

--- haskell/flake.nix
+++ haskell/flake.nix
@@ -2,50 +2,40 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    {
-      overlays = {
-        haskell = haskellSelf: haskellSuper: {
-          hello-world = haskellSelf.callCabal2nix "hello-world" ./. { };
-        };
-
-        default = final: prev: {
-          haskellPackages = prev.haskellPackages.override { overrides = self.overlays.haskell; };
-          haskell = prev.haskell // {
-            packages =
-              let
-                addPackages = _: compilerPackages: compilerPackages.override {
-                  overrides = self.overlays.haskell;
-                };
-              in
-              builtins.mapAttrs addPackages prev.haskell.packages;
-          };
-        };
+  outputs = { self, nixpkgs, flake-utils }: {
+    overlays = {
+      haskell = haskellSelf: haskellSuper: {
+        hello-world = haskellSelf.callCabal2nix "hello-world" ./. { };
       };
-    } // flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
 
-        cabalWrapped = pkgs.writeShellScriptBin "cabal" ''
-          ${pkgs.hpack}/bin/hpack && exec ${pkgs.cabal-install}/bin/cabal "$@"
-        '';
+      default = final: prev: {
+        haskellPackages = prev.haskellPackages.override { overrides = self.overlays.haskell; };
+      };
+    };
+  } // flake-utils.lib.eachDefaultSystem (
+    system:
+    let
+      pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
 
-        format-all = pkgs.writeShellScriptBin "format-all" ''
-          shopt -s globstar
-          ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt . && ${pkgs.ormolu}/bin/ormolu -i {app,src,test}/**/*.hs
-        '';
-      in
-      rec {
-        packages.default = pkgs.haskellPackages.hello-world;
+      cabalWrapped = pkgs.writeShellScriptBin "cabal" ''
+        ${pkgs.hpack}/bin/hpack && exec ${pkgs.cabal-install}/bin/cabal "$@"
+      '';
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ packages.default.env ];
-          packages = [
-            cabalWrapped
-            format-all
-          ];
-        };
-      }
-    );
+      format-all = pkgs.writeShellScriptBin "format-all" ''
+        shopt -s globstar
+        ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt . && ${pkgs.ormolu}/bin/ormolu -i {app,src,test}/**/*.hs
+      '';
+    in
+    rec {
+      packages.default = pkgs.haskellPackages.hello-world;
+
+      devShells.default = pkgs.mkShell {
+        inputsFrom = [ packages.default.env ];
+        packages = [
+          cabalWrapped
+          format-all
+        ];
+      };
+    }
+  );
 }
